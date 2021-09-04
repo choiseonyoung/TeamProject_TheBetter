92695b1d9e794c1d0b75afe8adba88a9acb72d2c
choiseonyoung <bviibb@naver.com>
2021-09-04 14:51:17 +0900

    아영언니바보

--- TheBetter/routes/dragRouter.js
+++ TheBetter/routes/dragRouter.js
@@ -9,9 +9,17 @@ router.get("/2", (req, res) => {
   res.render("drag2");
 });
 
+router.get("/3", (req, res) => {
+  res.render("drag3");
+});
+
 // https://runebook.dev/ko/docs/dom/datatransfer/effectallowed
 router.get("/test", (req, res) => {
   res.render("test");
+});
+
+router.get("/snap", (req, res) => {
+  res.render("snap");
 });
 
 module.exports = router;
